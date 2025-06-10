41441a57c10065802c6fc0332ad36637759d96a3
relobeid <ramezelobeid@icloud.com>
2025-06-10 16:45:29 -0400

    Added player control and camera follows car

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Private variables
+    private float speed = 5.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,7 +16,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       // Move the vehicle forward
-       transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        // Get the user's horizontal and vertical input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+       // Move the vehicle forward based on user input
+       transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+       // Rotate the vehicle based on user input
+       transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
