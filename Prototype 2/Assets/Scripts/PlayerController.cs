fa03f2d0238e5ca2b599685df8609787b724ddd8
relobeid <ramezelobeid@icloud.com>
2025-06-13 17:31:58 -0400

    Added object collision and random animal spawning, finishing game 2

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ public class PlayerController : MonoBehaviour
     // Player Movement
     public float horizontalInput;
     public float speed = 10.0f;
-    public float xRange = 10.0f;
+    public float xRange = 20.0f;
 
     public GameObject projectilePrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -20,13 +20,13 @@ public class PlayerController : MonoBehaviour
         // Keep the player in bounds - left side
         if(transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-20, transform.position.y, transform.position.z);
         }
 
         // Keep the player in bounds - right side
         if(transform.position.x > xRange)
         {
-            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(20, transform.position.y, transform.position.z);
         }
 
         // Move the player left and right
