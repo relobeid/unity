a407d1dc584f6592fe1ac4422a0700a71a36e5f5
relobeid <ramezelobeid@icloud.com>
2025-07-05 16:16:43 -0400

    powerups and enemy spawnig waves

--- Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -3,17 +3,26 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
     private float spawnRange = 9.0f;
+    public int enemyCount;
+    public int waveNumber = 1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsByType<Enemy>(FindObjectsSortMode.None).Length;
+        if(enemyCount == 0){
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
     }
 
     private Vector3 GenerateSpawnPosition(){
@@ -23,5 +32,11 @@ public class SpawnManager : MonoBehaviour
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
 
         return randomPos;
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn){
+        for (int i = 0; i < enemiesToSpawn; i++){
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
 }
